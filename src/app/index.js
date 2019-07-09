44232140ec68fca3b365b8fba68163f4e94ea8c9
DavidBalor <david.balderrama.dev@gmail.com>
2019-07-09 16:51:26 -0600

    get data from api

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,16 @@
 const ui = require('./ui'),
-      github = require('./github')
+      Github = require('./github'),
+      {client_id,client_secret} = require('./config.json')
+
+const github = new Github (client_id,client_secret)
+const userForm = document.querySelector('#userForm')
+
+userForm.addEventListener('submit', (e)=>{
+  const name = document.querySelector('#textSearch').value
+  
+  github.fetchUser(name)
+  .then(data => console.log(data))
+  e.preventDefault()
+})
 
       
