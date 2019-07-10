8b56eb554bbe4f8374876d663a1c9f1b5bdc8a5a
DavidBalor <david.balderrama.dev@gmail.com>
2019-07-10 15:34:07 -0600

    Finish course

--- src/app/github.js
+++ src/app/github.js
@@ -2,11 +2,18 @@ class Github {
   constructor (clientId,clientSecret) {
     this.client_id = clientId
     this.client_secret = clientSecret
+    this.repos_count = 10
+    this.repo_sort = "created: asc"
   }
   async fetchUser (user) {
     const userDataRequest = await fetch(`http://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+    const reposRequest = await fetch(`http://api.github.com/users/${user}/repos?client_id=${this.client_id}&client_secret=${this.client_secret}&per_page=${this.repos_count}&sort=${this.repo_sort}`)
     const userData = await userDataRequest.json()
-    return userData
+    const reposData = await reposRequest.json()
+    return {
+      userData:userData,
+      reposData:reposData
+    };
   }
 }
 
--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,24 @@
-const ui = require('./ui'),
+const UI = require('./ui'),
       Github = require('./github'),
       {client_id,client_secret} = require('./config.json')
 
 const github = new Github (client_id,client_secret)
 const userForm = document.querySelector('#userForm')
+const ui = new UI()
 
 userForm.addEventListener('submit', (e)=>{
   const name = document.querySelector('#textSearch').value
   
   github.fetchUser(name)
-  .then(data => console.log(data))
+  .then(data => {
+    if(/not found/i.test(data.userData.message)){
+      ui.showMessage(data.userData.message,'alert alert-danger col-md-12 mt-2')
+    } else {
+      console.log(data.userData)
+      ui.showProfile(data.userData)
+      ui.showRepositories(data.reposData)
+    }
+  })
   e.preventDefault()
 })
 
